9ff9b969e88dda6aca159166f2237a29bbf9955b
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-01-11 16:08:24 -0300

    criando rotas

---
+++ crud-angular-material/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./pages/search/search.component').then((m) => m.SearchComponent),
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'register' },
+  { path: '**', redirectTo: 'register' },
+];
