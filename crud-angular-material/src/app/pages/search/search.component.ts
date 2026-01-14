03e9e39ba757357e66443d0246d570a93ad8aaea
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-01-14 15:44:14 -0300

    criando tbl

--- crud-angular-material/src/app/pages/search/search.component.ts
+++ crud-angular-material/src/app/pages/search/search.component.ts
@@ -1,11 +1,24 @@
+import { MatTableModule } from '@angular/material/table';
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+  ],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
 })
-export class SearchComponent {
-
-}
+export class SearchComponent {}
