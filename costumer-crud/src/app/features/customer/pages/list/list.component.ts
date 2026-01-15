19d94f674b5ab8614237f1656fdd9d6ca9de6874
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-01-15 17:00:22 -0300

    tabela reesponsiva

--- costumer-crud/src/app/features/customer/pages/list/list.component.ts
+++ costumer-crud/src/app/features/customer/pages/list/list.component.ts
@@ -1,20 +1,24 @@
+import { CustomerService } from './../../../../services/customer.service';
 import { Component, inject, OnInit } from '@angular/core';
 import { CardModule } from '../../../../shared/modules/card/card.module';
 import { FormModule } from '../../../../shared/modules/form/form.module';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ButtonsModule } from '../../../../shared/modules/buttons/buttons.module';
 import { Customer } from '../register/register';
+import { CUSTOMER_ROUTES_NAME } from '../../routes/routes.model';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-list',
-  imports: [CardModule, FormModule, ButtonsModule],
+  imports: [CardModule, FormModule, ButtonsModule, RouterModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent implements OnInit {
   private fb = inject(FormBuilder);
   public customerList: Customer[] = [];
-
+  router = inject(Router);
+  customerService = inject(CustomerService);
   form = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(2)]],
   });
@@ -23,5 +27,17 @@ export class ListComponent implements OnInit {
     console.log(this.form);
   }
 
-  ngOnInit(): void {}
+  registerCustomer() {
+    this.router.navigate([
+      CUSTOMER_ROUTES_NAME.BASE_URL,
+      CUSTOMER_ROUTES_NAME.REGISTER,
+    ]);
+  }
+
+  getCustomers() {
+    this.customerList = this.customerService.getStorage();
+  }
+  ngOnInit(): void {
+    this.getCustomers();
+  }
 }
