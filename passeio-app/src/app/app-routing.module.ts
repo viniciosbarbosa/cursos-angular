bab542f49bb6191e3d2b1ff77111426a36277897
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-01-16 12:11:16 -0300

    passeio-app rotas

---
+++ passeio-app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'paginas',
+    loadChildren: () =>
+      import('./template/template.module').then((m) => m.TemplateModule),
+  },
+  // { path: '', pathMatch: 'full', redirectTo: 'paginas' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
