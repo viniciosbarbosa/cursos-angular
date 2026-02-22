10976b98bf2ee77960a495d24c6db3eb5c164a40
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-02-22 16:14:17 -0300

    first commit

---
+++ amazin-animals/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./product/product.module').then((m) => m.ProductModule),
+  },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ amazin-animals/src/app/product/product.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductRoutingModule } from './product-routing.module';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+@NgModule({
+  declarations: [ProductListComponent],
+  imports: [CommonModule, ProductRoutingModule],
+})
+export class ProductModule {}
