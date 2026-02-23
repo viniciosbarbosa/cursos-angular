832da54367cfcc55c9f554a5a46bb73ab5d06a9a
Vinicios Barbosa <viniciosbarbosagit@gmail.com>
2026-02-22 21:44:37 -0300

    service

--- amazing-animals/src/app/cart/service/cart.service.ts
+++ amazing-animals/src/app/cart/service/cart.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../product/models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  private apiUrl = 'http://localhost:3000  ';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCartItems(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}/cart`);
+  }
+
+  addToCart(product: Product): Observable<Product> {
+    return this.http.post<Product>(`${this.apiUrl}/cart`, product);
+  }
+
+  cleanCart(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/cart`);
+  }
 }
